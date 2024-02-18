1e2741c8bb6560c6da287b1677a85280c0ab5dbb
fares-sonar <faressonar678@gmai.com>
2024-02-18 19:34:52 +0300

    delete the dist image

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import {Hero} from "../section/index"
 const Container = () => {
   return (
     <>
-      <section className=" relative bg-[url('./dist/images/banner-bg.jpg')] bg-no-repeat bg-cover h-[150vh] md:h-[100vh] rounded-b-[135px] -z-10">
+      <section className=" relative bg-[url('./images/banner-bg.jpg')] bg-no-repeat bg-cover h-[150vh] md:h-[100vh] rounded-b-[135px] -z-10">
         <Hero/>
       </section>
     </>
