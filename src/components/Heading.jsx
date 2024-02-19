414ef8f3fb6dadb0d9219fbe1a21e0d20045b2f6
fares-sonar <faressonar678@gmai.com>
2024-02-19 21:43:13 +0300

    render hero bg

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = (prop) => {
   return (
     <>
-      <div className="bg-[url('./src/images/page-heading-bg.jpg')] bg-no-repeat bg-cover w-full h-[45vh] flex flex-col gap-12 justify-center items-center rounded-b-[135px] mb-28 ">
+      <div className="bg-[url('/src/images/page-heading-bg.jpg')] bg-no-repeat bg-cover w-full h-[45vh] flex flex-col gap-12 justify-center items-center rounded-b-[135px] mb-28 ">
         <h1 className="text-white text-5xl font-extrabold uppercase">{prop.title}</h1>
         <p className="text-white font-medium text-lg">Home &gt; <span className="capitalize">{prop.text}</span></p>
       </div>
